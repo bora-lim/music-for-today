80ed6e0f9efbb8fd41b037b98cc4801c65b1bde8
bora-lim <yksysr@gmail.com>
2021-05-07 16:29:57 +0900

    test

---
+++ src/components/weather.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./weather.scss";
+
+const Weather = (props) => {
+  const getInfo = () => {
+    console.log("click!");
+
+    const url =
+      "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst"; /*URL*/
+    let queryParams =
+      "?" +
+      encodeURIComponent("ServiceKey") +
+      "=" +
+      process.env.REACT_APP_API_KEY; /*Service Key*/
+    queryParams +=
+      "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
+    queryParams +=
+      "&" +
+      encodeURIComponent("numOfRows") +
+      "=" +
+      encodeURIComponent("10"); /**/
+    queryParams +=
+      "&" +
+      encodeURIComponent("dataType") +
+      "=" +
+      encodeURIComponent("XML"); /**/
+    queryParams +=
+      "&" +
+      encodeURIComponent("base_date") +
+      "=" +
+      encodeURIComponent("20210507"); /**/
+    queryParams +=
+      "&" +
+      encodeURIComponent("base_time") +
+      "=" +
+      encodeURIComponent("1500"); /**/
+    queryParams +=
+      "&" + encodeURIComponent("nx") + "=" + encodeURIComponent("18"); /**/
+    queryParams +=
+      "&" + encodeURIComponent("ny") + "=" + encodeURIComponent("1"); /**/
+
+    fetch(`${url}${queryParams}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(JSON.stringify(myJson));
+      });
+  };
+
+  return <button onClick={getInfo}>click</button>;
+};
+
+export default Weather;
